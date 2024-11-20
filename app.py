071f2b63919ae3a25dcd37b1a2516659e188cf44
Rosalia Mazza <rosaliamazza.comm@gmail.com>
2024-11-20 17:46:00 +0100

    Adding elements to the sidebar

--- app.py
+++ app.py
@@ -6,3 +6,11 @@ import plotly.graph_objects as go
 
 st.title("Market Dashboard Application")
 st.sidebar.header("User Input")
+
+def get_input():
+    symbol = st.sidbar.text_input("Symbol", "BTC-USD")
+    start_date = st.sidebar.date_input("Start Date", "2021-01-01")
+    end_date = st.sidebar.date_input("End Date", "2021-12-31")
+
+    return symbol, start_date, end_date
+    
