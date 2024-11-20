a28af190c02f1caea2ae3800540bdd3422c56853
Rosalia Mazza <rosaliamazza.comm@gmail.com>
2024-11-20 18:01:51 +0100

    Adding elements to the sidebar

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-
 import pandas as pd
 import yfinance as yf
 import streamlit as st
 import plotly.graph_objects as go
+from datetime import date  # Import the date class
 
 st.title("Market Dashboard Application")
 st.sidebar.header("User Input")
 
 def get_input():
+    # Use date objects for default values
     symbol = st.sidebar.text_input("Symbol", "BTC-USD")
-    start_date = st.sidebar.date_input("Start Date", "2021-01-01")
-    end_date = st.sidebar.date_input("End Date", "2021-12-31")
+    start_date = st.sidebar.date_input("Start Date", date(2021, 1, 1))
+    end_date = st.sidebar.date_input("End Date", date(2021, 12, 31))
 
     return symbol, start_date, end_date
 
+# Unpack user inputs
 symbol, start_date, end_date = get_input()
