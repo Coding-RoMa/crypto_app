03dbbfb4bd28f47fc59d84adefda6c3b4adf8f33
Rosalia Mazza <rosaliamazza.comm@gmail.com>
2024-11-21 19:15:43 +0100

    Adding patterns

---
+++ patterns.py
@@ -0,0 +1,27 @@
+patterns = {
+'CDL2CROWS':'Two Crows',
+
+'CDL3BLACKCROWS' : 'Three Black Crows',
+
+'CDL3INSIDE' : 'Three Inside Up/Down',
+
+'CDL3LINESTRIKE' : 'Three:Line Strike', 
+
+'CDL3OUTSIDE' : 'Three Outside Up/Down',
+
+'CDL3STARSINSOUTH' : 'Three Stars In The South',
+
+'CDL3WHITESOLDIERS' : 'Three Advancing White Soldiers',
+
+'CDLABANDONEDBABY' : 'Abandoned Baby',
+
+'CDLADVANCEBLOCK' : 'Advance Block',
+
+'CDLBELTHOLD' : 'Belt hold',
+
+'CDLBREAKAWAY' : 'Breakaway',
+
+'CDLCLOSINGMARUBOZU' : 'Closing Marubozu'
+
+
+}
